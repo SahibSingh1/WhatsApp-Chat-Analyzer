c057280974d5225964a58b7d3d1931c3fcded6eb
Sahib Singh <71924657+sahibsingh03@users.noreply.github.com>
2022-04-05 12:19:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import preprocessor
 import helper
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.sidebar.title("WhatsApp Chat Analyzer")
 uploaded_file = st.sidebar.file_uploader("Choose a file")
