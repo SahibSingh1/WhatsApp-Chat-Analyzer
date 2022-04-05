7910538ebc363e93a1eff7c29d51eebe9a961552
Sahib Singh <71924657+sahibsingh03@users.noreply.github.com>
2022-04-05 14:30:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     data = bytes_data.decode("utf-8")
-    df = preprocessor.preprocess(data)
+    df = preprocessor.processor(data)
 
     # fetch unique users
     user_list = df['user'].unique().tolist()
-    user_list.remove('group_notification')
+    user_list.remove('Notification')
     user_list.sort()
     user_list.insert(0,"Overall")
 
@@ -24,7 +24,7 @@ if uploaded_file is not None:
         # Stats Area
         num_messages, words, num_media_messages, num_links = helper.fetch_stats(selected_user,df)
         st.title("Top Statistics")
-        col1, col2, col3, col4 = st.beta_columns(4)
+        col1, col2, col3, col4 = st.columns(4)
 
         with col1:
             st.header("Total Messages")
@@ -43,7 +43,7 @@ if uploaded_file is not None:
         st.title("Monthly Timeline")
         timeline = helper.monthly_timeline(selected_user,df)
         fig,ax = plt.subplots()
-        ax.plot(timeline['time'], timeline['message'],color='green')
+        ax.plot(timeline['time'], timeline['messages'],color='green')
         plt.xticks(rotation='vertical')
         st.pyplot(fig)
 
@@ -51,17 +51,17 @@ if uploaded_file is not None:
         st.title("Daily Timeline")
         daily_timeline = helper.daily_timeline(selected_user, df)
         fig, ax = plt.subplots()
-        ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='black')
+        ax.plot(daily_timeline['date_only'], daily_timeline['messages'], color='black')
         plt.xticks(rotation='vertical')
         st.pyplot(fig)
 
         # activity map
         st.title('Activity Map')
-        col1,col2 = st.beta_columns(2)
+        col1,col2 = st.columns(2)
 
         with col1:
             st.header("Most busy day")
-            busy_day = helper.week_activity_map(selected_user,df)
+            busy_day = helper.weekly_activity(selected_user,df)
             fig,ax = plt.subplots()
             ax.bar(busy_day.index,busy_day.values,color='purple')
             plt.xticks(rotation='vertical')
@@ -69,7 +69,7 @@ if uploaded_file is not None:
 
         with col2:
             st.header("Most busy month")
-            busy_month = helper.month_activity_map(selected_user, df)
+            busy_month = helper.monthly_activity(selected_user, df)
             fig, ax = plt.subplots()
             ax.bar(busy_month.index, busy_month.values,color='orange')
             plt.xticks(rotation='vertical')
@@ -84,10 +84,10 @@ if uploaded_file is not None:
         # finding the busiest users in the group(Group level)
         if selected_user == 'Overall':
             st.title('Most Busy Users')
-            x,new_df = helper.most_busy_users(df)
+            x,new_df = helper.busy_users(df)
             fig, ax = plt.subplots()
 
-            col1, col2 = st.beta_columns(2)
+            col1, col2 = st.columns(2)
 
             with col1:
                 ax.bar(x.index, x.values,color='red')
@@ -95,16 +95,16 @@ if uploaded_file is not None:
                 st.pyplot(fig)
             with col2:
                 st.dataframe(new_df)
-
+        
         # WordCloud
         st.title("Wordcloud")
-        df_wc = helper.create_wordcloud(selected_user,df)
+        df_wc = helper.wordcloud(selected_user,df)
         fig,ax = plt.subplots()
         ax.imshow(df_wc)
         st.pyplot(fig)
 
         # most common words
-        most_common_df = helper.most_common_words(selected_user,df)
+        most_common_df = helper.most_common(selected_user,df)
 
         fig,ax = plt.subplots()
 
@@ -115,10 +115,10 @@ if uploaded_file is not None:
         st.pyplot(fig)
 
         # emoji analysis
-        emoji_df = helper.emoji_helper(selected_user,df)
+        emoji_df = helper.top_emoji(selected_user,df)
         st.title("Emoji Analysis")
 
-        col1,col2 = st.beta_columns(2)
+        col1,col2 = st.columns(2)
 
         with col1:
             st.dataframe(emoji_df)
